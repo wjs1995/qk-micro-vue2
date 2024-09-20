4281ecc788b82ba14e04c4e0c7df07247ef67546
wangjinsheng <1334258066@qq.com>
2024-09-20 16:46:01 +0800

    子应用通了

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    123
     <router-view />
   </div>
 </template>
